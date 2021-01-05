4f96170227cd2316fffa8c2ccfb11e0128e99c8e
tanpm <tanpm@mobiletech.vn>
2021-01-05 07:00:37 +0700

    aa

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
 
 Bundler.require(*Rails.groups)
 
@@ -17,6 +16,7 @@ module RedmineApp
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+    config.assets.initialize_on_precompile = false
 
     config.active_record.store_full_sti_class = true
     config.active_record.default_timezone = :local
